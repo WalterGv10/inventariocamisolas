ba5c7a34c0969f69bed30b26cb33c4acb91753b1
Walter García Véliz <wgarcia.10@outlook.com>
2026-01-18 00:25:46 -0600

    Fix: Add Typescript declarations for CSS modules and Vite env

---
+++ src/vite-env.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_SUPABASE_URL: string;
+    readonly VITE_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
+
+declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
+
+declare module '*.svg' {
+    import * as React from 'react';
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+    const src: string;
+    export default src;
+}
